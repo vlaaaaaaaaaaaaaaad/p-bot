cb7da25edb7fc6dfd39c086ca8656c4ef153dc7b
vlaaaaaaaaaaaaaaad <130173359+vlaaaaaaaaaaaaaaad@users.noreply.github.com>
2024-05-06 22:34:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,54 @@
-/**
- * @author Andrey Nedobylskiy (admin@twister-vl.ru)
- *
- */
-
 const puppeteer = require('puppeteer');
 
-/**
- * Класс, позволяющий делать запросы в pBot и получать ответы
- */
 class PBot {
-    /**
-     *
-     * @param botName Name for bot. Mr. Potato for ex.
-     * @param lang Bot language en, ru
-     */
     constructor(botName = 'ХахБот', lang = 'ru') {
         this.botName = botName;
         this.page = null;
         this.queue = [];
-        this.lang = lang
+        this.lang = lang;
+        this.browser = null;
     }
 
-    /**
-     * Сказать в чат и полчить ответ
-     * @param text
-     * @returns {Promise<string>}
-     */
     async _sayToBot(text) {
         let result = await this.page.evaluate((text) => {
-            return new Promise((resolve) => {
+            return new Promise((resolve, reject) => {
                 $('.last_answer').text('NOTEXT');
                 $('.main_input').val(text);
                 $('#btnSay').click();
 
                 let lastAnswer = '';
-
                 let timer = setInterval(() => {
-
-                    if($('.last_answer').text() && $('.last_answer').text() !== 'NOTEXT' && $('.last_answer').text() !== 'ρBot: думаю...' && $('.last_answer').text() !== 'ρBot: thinking...') {
-                        if(lastAnswer === $('.last_answer').text()) {
+                    if ($('.last_answer').text() && $('.last_answer').text() !== 'NOTEXT' && $('.last_answer').text() !== 'ρBot: думаю...' && $('.last_answer').text() !== 'ρBot: thinking...') {
+                        if (lastAnswer === $('.last_answer').text()) {
                             clearInterval(timer);
                             resolve($('.last_answer').text());
                         }
                         lastAnswer = $('.last_answer').text();
                     }
                 }, 100);
-
-            })
+                setTimeout(() => {
+                    clearInterval(timer);
+                    reject(new Error("Timeout Error"));
+                }, 10000);  // 10 секунд таймаут
+            });
         }, text);
-
         result = result.split(':')[1].trim();
-
         result = result.replace(/pBot/g, this.botName);
         result = result.replace(/ρBot/g, this.botName);
-
         return result;
-
     }
 
-    /**
-     * Сказать боту и получить ответ
-     * @param {string} text
-     * @returns {Promise<string>}
-     * @async
-     */
-    say(text) {
-        return new Promise((resolve => {
-            this.queue.push({text, cb: (response) => resolve(response)});
-        }))
+    async say(text) {
+        return new Promise((resolve, reject) => {
+            this.queue.push({ text, cb: (response) => resolve(response), err: reject });
+        });
     }
 
-    /**
-     * Инициализация
-     * @param {*} options
-     * @returns {Promise<void>}
-     */
-    async init(options = {headless: true}) {
+    async init(options = { headless: true }) {
         this.browser = await puppeteer.launch(options);
-
         this.page = await this.browser.newPage();
         await this.page.setDefaultNavigationTimeout(0);
 
-        //Выбор языка
         switch (this.lang) {
             case "en":
                 await this.page.goto('http://p-bot.ru/en/index.html');
@@ -91,33 +58,34 @@ class PBot {
                 await this.page.goto('http://p-bot.ru/');
         }
 
-        //await page.screenshot({path: 'example.png'});
-        await this.page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
+        await this.page.addScriptTag({ url: 'https://code.jquery.com/jquery-3.2.1.min.js' });
 
-        //Таймер очереди запросов
         const queueProcesser = async () => {
             let request = this.queue.shift();
-
-            if(!request) {
+            if (!request) {
                 this.queueTimer = setTimeout(queueProcesser, 100);
                 return;
             }
-
-            //Выполняем обработку запроса
-            request.cb(await this._sayToBot(request.text));
-
+            try {
+                request.cb(await this._sayToBot(request.text));
+            } catch (error) {
+                request.err(error);
+                // Переподключение при ошибке
+                await this.destroy();
+                await this.init(options);
+            }
             this.queueTimer = setTimeout(queueProcesser, 100);
         };
         this.queueTimer = setTimeout(queueProcesser, 100);
     }
 
-    /**
-     * Остановка
-     * @returns {Promise<void>}
-     */
     async destroy() {
         clearTimeout(this.queueTimer);
-        await this.browser.close();
+        if (this.browser) {
+            await this.browser.close();
+        }
+        this.browser = null;
+        this.page = null;
     }
 }
 
