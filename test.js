d4aa69f2b3ef5af2515accf0281f5ffddff6413e
vlaaaaaaaaaaaaaaad <130173359+vlaaaaaaaaaaaaaaad@users.noreply.github.com>
2024-05-06 21:15:20 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,24 +2,34 @@ const express = require('express');
 const PBot = require('./index');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Создаем экземпляр PBot
 const pBot = new PBot('ХахБот', 'ru');
 
-// Инициализируем PBot при запуске сервера
-app.listen(port, async () => {
+// Инициализация бота
+async function initBot() {
     await pBot.init();
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log('Bot is initialized');
+}
+
+// Запуск сервера
+app.listen(PORT, async () => {
+    await initBot();
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Обработчик POST запросов на /ask
+// Корневой маршрут для проверки живучести сервера
+app.get('/', (req, res) => {
+    res.send("I'm alive");
+});
+
+// Маршрут для обработки POST запросов на /ask
 app.post('/ask', express.json(), async (req, res) => {
     const { text } = req.body;
     if (!text) {
         return res.status(400).send('Text is required');
     }
-
     try {
         const response = await pBot.say(text);
         res.send(response);
@@ -29,7 +39,7 @@ app.post('/ask', express.json(), async (req, res) => {
     }
 });
 
-// Завершаем работу PBot при остановке сервера
+// Обработка сигнала остановки для корректного закрытия бота
 process.on('SIGINT', async () => {
     await pBot.destroy();
     console.log('Server stopped');
