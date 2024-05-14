21e1c033927c427a2b4b8fee7d5e03872c5aac50
vlaaaaaaaaaaaaaaad <130173359+vlaaaaaaaaaaaaaaad@users.noreply.github.com>
2024-05-14 20:27:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,51 +4,14 @@ class PBot {
     constructor(botName = 'ХахБот', lang = 'ru') {
         this.botName = botName;
         this.page = null;
-        this.queue = [];
         this.lang = lang;
         this.browser = null;
     }
 
-    async _sayToBot(text) {
-        let result = await this.page.evaluate((text) => {
-            return new Promise((resolve, reject) => {
-                $('.last_answer').text('NOTEXT');
-                $('.main_input').val(text);
-                $('#btnSay').click();
-
-                let lastAnswer = '';
-                let timer = setInterval(() => {
-                    if ($('.last_answer').text() && $('.last_answer').text() !== 'NOTEXT' && $('.last_answer').text() !== 'ρBot: думаю...' && $('.last_answer').text() !== 'ρBot: thinking...') {
-                        if (lastAnswer === $('.last_answer').text()) {
-                            clearInterval(timer);
-                            resolve($('.last_answer').text());
-                        }
-                        lastAnswer = $('.last_answer').text();
-                    }
-                }, 100);
-                setTimeout(() => {
-                    clearInterval(timer);
-                    reject(new Error("Timeout Error"));
-                }, 10000);  // 10 секунд таймаут
-            });
-        }, text);
-        result = result.split(':')[1].trim();
-        result = result.replace(/pBot/g, this.botName);
-        result = result.replace(/ρBot/g, this.botName);
-        return result;
-    }
-
-    async say(text) {
-        return new Promise((resolve, reject) => {
-            this.queue.push({ text, cb: (response) => resolve(response), err: reject });
-        });
-    }
-
-    async init(options = { headless: true }) {
-        this.browser = await puppeteer.launch(options);
+    async init() {
+        this.browser = await puppeteer.launch({ headless: true });
         this.page = await this.browser.newPage();
         await this.page.setDefaultNavigationTimeout(0);
-
         switch (this.lang) {
             case "en":
                 await this.page.goto('http://p-bot.ru/en/index.html');
@@ -57,35 +20,43 @@ class PBot {
             default:
                 await this.page.goto('http://p-bot.ru/');
         }
-
         await this.page.addScriptTag({ url: 'https://code.jquery.com/jquery-3.2.1.min.js' });
+    }
 
-        const queueProcesser = async () => {
-            let request = this.queue.shift();
-            if (!request) {
-                this.queueTimer = setTimeout(queueProcesser, 100);
-                return;
-            }
-            try {
-                request.cb(await this._sayToBot(request.text));
-            } catch (error) {
-                request.err(error);
-                // Переподключение при ошибке
-                await this.destroy();
-                await this.init(options);
-            }
-            this.queueTimer = setTimeout(queueProcesser, 100);
-        };
-        this.queueTimer = setTimeout(queueProcesser, 100);
+    async say(text) {
+        try {
+            const result = await this.page.evaluate((text) => {
+                return new Promise((resolve, reject) => {
+                    $('.last_answer').text('NOTEXT');
+                    $('.main_input').val(text);
+                    $('#btnSay').click();
+
+                    let lastAnswer = '';
+                    let timer = setInterval(() => {
+                        if ($('.last_answer').text() && $('.last_answer').text() !== 'NOTEXT' && $('.last_answer').text() !== 'ρBot: думаю...' && $('.last_answer').text() !== 'ρBot: thinking...') {
+                            if (lastAnswer === $('.last_answer').text()) {
+                                clearInterval(timer);
+                                resolve($('.last_answer').text());
+                            }
+                            lastAnswer = $('.last_answer').text();
+                        }
+                    }, 100);
+                    setTimeout(() => {
+                        clearInterval(timer);
+                        reject(new Error("Timeout Error"));
+                    }, 10000);  // 10 секунд таймаут
+                });
+            }, text);
+            return result.split(':')[1].trim().replace(/pBot/g, this.botName).replace(/ρBot/g, this.botName);
+        } finally {
+            await this.destroy();
+        }
     }
 
     async destroy() {
-        clearTimeout(this.queueTimer);
-        if (this.browser) {
-            await this.browser.close();
-        }
-        this.browser = null;
+        await this.browser.close();
         this.page = null;
+        this.browser = null;
     }
 }
 
